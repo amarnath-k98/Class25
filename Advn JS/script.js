1dd048660ccc28d3f45218ec90ae773248a735f8
amarnath-98 <amarmagesh717@gmail.com>
2025-07-18 20:09:15 +0530

    event js

--- Advn JS/script.js
+++ Advn JS/script.js
@@ -31,3 +31,45 @@ contain.innerHTML = `
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error tenetur magni labore delectus voluptatibus! Magnam doloremque unde sapiente. Voluptatum non earum sed sunt veritatis dolor harum molestiae, veniam cum atque.</p>
     <button>Save</button>
 `;
+
+
+
+const input = document.querySelector("input");
+const toogleBtn = document.querySelector("#pass-check");
+
+function toogleEvent() {
+    const inputType = input.getAttribute("type");
+    if (inputType == "password") {
+        input.setAttribute("type", "text");
+        toogleBtn.innerText ="Hide";
+    } else {
+        input.setAttribute("type", "password");
+        toogleBtn.innerText ="Show";   
+    }
+}
+
+toogleBtn.addEventListener("click", () => {
+    toogleEvent();
+});
+
+function getAndInvoke(nameOfFn, fn) {
+    console.log(nameOfFn);
+    fn();
+}
+
+function someFn(name) {
+    console.log("someFn is called by", name);
+}
+getAndInvoke("tickFn", ()=> {
+    someFn("Amar");
+});
+
+document.addEventListener("mouseup",()=> {
+    console.log("event mouse triggerd");
+});
+
+const myForm = document.querySelector("form");
+myForm.addEventListener("input", (e) => {
+    e.preventDefault();
+    console.log("input has triggerd in form")
+});
