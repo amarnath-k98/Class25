e33f6e4c9870b2e72c3f4ad612822dd3ba5a3cf6
amarnath-98 <amarmagesh717@gmail.com>
2025-07-21 20:30:21 +0530

    start

---
+++ Advn JS/JS-Project/script.js
@@ -0,0 +1,40 @@
+let formContainer = document.createElement("div");
+let mainContainer = document.getElementById("main");
+formContainer.setAttribute("id","form-box");
+formContainer.innerHTML += `
+<form class=" shadow-2xl w-[700px] h-[400px] flex justify-center items-center flex-col gap-3">
+<h1 class="text-2xl font-bold">Student Portal Form</h1>
+ <input required type="text" id="name-input" class="focus:outline-none p-4 bg-slate-200 rounded-md w-[90%]" placeholder="Enter Student name"/>
+ <input required type="text" id="batch-input" class="focus:outline-none p-4 bg-slate-200 rounded-md w-[90%]" placeholder="Enter Batch name"/>
+ <input required type="text" id="course-input" class="focus:outline-none p-4 bg-slate-200 rounded-md w-[90%]" placeholder="Enter Course name"/>
+ <button id="save-btn" class="bg-blue-500 p-2 w-36 rounded-sm text-black font-semibold">Save</button>
+
+</form>
+`;
+
+mainContainer.append(formContainer);
+let nameInput = document.getElementById("name-input");
+let batchInput = document.getElementById("batch-input");
+let courseInput = document.getElementById("course-input");
+const saveBtn = document.getElementById("save-btn");
+
+function removeInputField() {
+    nameInput.value = "";
+    batchInput.value = "";
+    courseInput.value = "";
+}
+
+formContainer.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const newStudentObj = {
+        student_name: nameInput?.value,
+        student_batch: batchInput?.value,
+        student_course: courseInput?.value,
+    };
+    if (newStudentObj) {
+        console.log(newStudentObj);
+        removeInputField();
+    }
+});
+
+
